41d1ab6347dcfa3f25037a75970aef985347ff22
Meghnakrishnan <74606204+Meghnakrishnan@users.noreply.github.com>
2021-05-04 20:51:48 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,26 @@
+class Arithmetic {
+        public static void main(String args[]) {
+        int a,b,add,subtract,multiply,division;		//variable declaration
+        a=10;		//assigning value to variable
+        b=5; 
+        add=a + b;	//add 2 numbers and assign in 'add'
+        subtract=a - b;
+        multiply=a * b;
+        division=a / b;
+        
+        System.out.println("Sum = " + add); 	//to print results
+        System.out.println("Difference = " + subtract);
+        System.out.println("Multiplication = " + multiply);
+        System.out.println("Division = " + division);
+        }
+}
+
+
+/*output
+
+Sum = 15
+Difference = 5
+Multiplication = 50
+Division = 2
+
+*/
