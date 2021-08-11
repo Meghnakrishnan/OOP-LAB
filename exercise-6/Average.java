50cc9c1aabef3007b9b097d96e0921a2cc60432c
Meghnakrishnan <74606204+Meghnakrishnan@users.noreply.github.com>
2021-08-11 21:45:38 +0530

    Add files via upload

---
+++ exercise-6/Average.java
@@ -0,0 +1,62 @@
+/*Find the average of N positive integers, raising a user defined exception for each negative input.*/
+
+import java.util.Scanner;
+import java.util.*;
+class AverageException extends Exception
+{
+	AverageException(String str)
+	{
+		super(str);
+	}
+}
+class Average{
+public static void main (String args[]){
+int sum=0;
+Scanner sc=new Scanner(System.in);
+System.out.print("Enter the limit :");							//ENTER NO: OF INTEGERS
+int num=sc.nextInt();									//STORE IN 'num'
+int[] arr=new int[num];									//ARRAY DECLARATION OF SIZE 'num'
+int i=0;
+while(i<num)										//LOOP EXECUTE ONLY WHEN CONDITION IS TRUE
+{
+	
+	int number=sc.nextInt();
+		try{
+			if(number<0)
+			{
+				throw new AverageException("negative input");
+			}
+			else
+			{
+				arr[i]=number;
+				i=i+1;
+			}
+		     }
+		catch(Exception e)
+	            {
+			System.out.println("negative numbers are not allowed .please enter a new number");
+			continue;
+		    }
+}
+for(i=0;i<num;i++)
+{
+	sum=sum+arr[i];
+}
+int avg=sum/num;
+System.out.println("Average of "+num+" positive numbers :"+avg);
+}
+}
+
+
+/* output
+
+Enter the limit :2
+8
+-5
+negative numbers are not allowed .please enter a new number
+5
+Average of 2 positive numbers :6
+
+*/
+
+				
---
+++ exercise-6/Login.java
@@ -0,0 +1,101 @@
+//Write a user defined exception class to authenticate the user name and password
+
+import java.util.Scanner;
+							
+class UnameException extends Exception						//UnameException class
+{
+  public UnameException(String msg)
+  {
+    super(msg);
+  }
+}
+
+class PwdException extends Exception						//PwdException class
+{
+ public PwdException(String msg)
+  {
+    super(msg);
+  }
+}
+
+
+
+class Login									//main class
+{
+  public static void main(String args[])
+  {
+     Scanner sn=new Scanner(System.in);
+     String uname,pwd;
+         
+   try
+    {
+	System.out.print("Enter new  username:");				
+     	uname=sn.nextLine();							//store username in 'uname'
+     	System.out.print("Enter new password:");
+     	pwd=sn.nextLine();							//store password in 'pwd'	
+	int len=uname.length();							//store length of 'uname' in 'len'
+     if(len<6)									
+        throw new UnameException("Username must contain minimum 6 characters");	//statemant executes when if condition is true
+     else{
+	  System.out.print("LOGIN\n*******");
+          String username,password;
+   	  System.out.print("\nEnter username:");
+   	  username=sn.nextLine();
+   	  System.out.print("Enter password:");
+   	  password=sn.nextLine();
+
+    	  if(pwd.equals(password) && uname.equals(username))			
+		System.out.println("Successfully login!");			//statemant execute only when both are true	
+     	  else 
+		throw new PwdException("Incorrect password! Try again!!");
+	 
+        	
+  	}
+    }
+    catch (UnameException e)
+    {
+       System.out.println(e);
+    }
+    catch (PwdException e)
+    {
+       System.out.println(e);
+    }
+   
+     finally{
+            System.out.println("The finally statement is executed");
+       }  
+
+   }
+}
+
+/* OUTPUT
+
+C:\Users\SNJ\Desktop\OOP\exercise-6>java Login
+Enter new  username:meghna
+Enter new password:43567
+LOGIN
+*******
+Enter username:meghna
+Enter password:4359
+PwdException: Incorrect password! Try again!!
+The finally statement is executed
+
+
+C:\Users\SNJ\Desktop\OOP\exercise-6>java Login
+Enter new  username:megha
+Enter new password:43567
+UnameException: Username must contain minimum 6 characters
+The finally statement is executed
+
+
+C:\Users\SNJ\Desktop\OOP\exercise-6>java Login
+Enter new  username:meghna
+Enter new password:43567
+LOGIN
+*******
+Enter username:meghna
+Enter password:43567
+Successfully login!
+The finally statement is executed
+
+*/
