22396492d18c08f27bbc398234e1cca04a5ea4af
Meghnakrishnan <74606204+Meghnakrishnan@users.noreply.github.com>
2021-09-27 10:47:11 +0530

    Add files via upload

---
+++ exercise-12/q1/MyServer.java
@@ -0,0 +1,29 @@
+//1. Client server communication using Socket – TCP/IP
+
+
+import java.io.*;
+import java.net.*;
+public class MyServer {
+
+	public static void main(String[] args){
+		try{
+
+			ServerSocket ss=new ServerSocket(6666);
+			Socket s=ss.accept();//establishes connection
+			DataInputStream dis=new DataInputStream(s.getInputStream());
+			String str=(String)dis.readUTF();
+			System.out.println("message= "+str);
+			ss.close();
+
+		}catch(Exception e){System.out.println(e);}
+	}
+}
+
+/*
+
+output
+
+C:\Users\SNJ\Desktop\OOP\exercise-12\q1>java MyServer
+message= WELCOME JAVA
+
+*/
---
+++ exercise-12/q2/DatagramReceiver.java
@@ -0,0 +1,22 @@
+//2. Client Server communication using DatagramSocket - UDP
+
+import java.net.*;
+public class DatagramReceiver{
+public static void main(String[] args) throws Exception {
+DatagramSocket ds = new DatagramSocket(3000);
+byte[] buf = new byte[1024];
+DatagramPacket dp = new DatagramPacket(buf, 1024);
+ds.receive(dp);
+String str = new String(dp.getData(), 0, dp.getLength());
+System.out.println(str);
+ds.close();
+
+}
+}
+
+/*
+output:
+-------
+Welcome java
+
+*/
